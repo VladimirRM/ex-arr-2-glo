a2cbf9eb0f20a447488b0f2ea82a92a9362f8938
Vladimir RM <shvdrummer@gmail.com>
2023-02-22 19:00:01 +0200

    concat strict dona

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 const number = [12, -3, 56, 76, 45, 0];
-const names = ["vladimir", "Mark", "Kate", "Margo", "Peter"];
+const names = ["vladimir", "Mark", "kate", "Margo", "Peter"];
 const mix = [true, 0, NaN, -1, "Vladimir"];
 const BadNum = [45, -7, "Mark", 2, 5, 7, 8];
 
-// for (let i = 0; 0 < mix.length; i++) {
-
-// }
-
-mix.forEach( (item, index, arr)=> {
-  console.table({ item, index, arr });
-});
+for (let i = 0; i < names.length; i++) {
+  names[i] = names[i][0].toUpperCase() + names[i].slice(1).toLowerCase();
+}
+console.log(names);
