bda572b0e2ffb4423d097aafcacae33835479ec7
Vladimir RM <shvdrummer@gmail.com>
2023-02-22 19:04:52 +0200

    conCat foreach done

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const names = ["vladimir", "Mark", "kate", "Margo", "Peter"];
 const mix = [true, 0, NaN, -1, "Vladimir"];
 const BadNum = [45, -7, "Mark", 2, 5, 7, 8];
 
-for (let i = 0; i < names.length; i++) {
-  names[i] = names[i][0].toUpperCase() + names[i].slice(1).toLowerCase();
-}
+names.forEach(function (item, i, arr) {
+  arr[i] = item[0].toUpperCase(1) + item.slice(1).toLowerCase();
+});
+
 console.log(names);
