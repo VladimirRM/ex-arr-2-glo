e2a219276f5ddd9cbc7326509330a8972e81375f
Vladimir RM <shvdrummer@gmail.com>
2023-02-22 18:22:36 +0200

    create array for ex

---
+++ script.js
@@ -0,0 +1,4 @@
+const number = [12, -3, 56, 76, 45, 0];
+const names = ["vladimir", "Mark", "Kate", "Margo", "Peter"];
+const mix = [true, 0, NaN, -1,'Vladimir']
+const BadNum = [45,-7,'Mark',2,5,7,8,]
