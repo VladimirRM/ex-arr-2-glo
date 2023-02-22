fb1fb2ebe68f7ae87b795f73c28337f44426f675
Vladimir RM <shvdrummer@gmail.com>
2023-02-22 21:03:07 +0200

    selected boolean done

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 const number = [12, -3, 56, 76, 45, 0];
 const names = ["vladimir", "Mark", "kate", "Margo", "Peter"];
-const mix = [true, 0, NaN, -1, "Vladimir", "Khoma"];
+const mix = [false, 0, NaN, 1, "Vladimir", "Khoma", 12];
 const BadNum = [45, -7, "Mark", 2, 5, 7, 8];
 
-let forWords = [];
 
-for (let i = 0; i < mix.length; i++) {
-  if (typeof mix[i] === "string") {
-    forWords.push(mix[i]);
-  }
-}
-
-console.log(forWords);
--- script1.js
+++ script1.js
@@ -11,23 +11,18 @@
 //   console.table({ item, index, arr });
 // });
 
-
 ////////////// For LOwerCase /////////////////
-
 
 // for (let i = 0; i < names.length; i++) {
 //   names[i] = names[i][0].toUpperCase() + names[i].slice(1).toLowerCase();
 // }
 // console.log(names);
 
-
 // names.forEach(function (item, i, arr) {
 //   arr[i] = item[0].toUpperCase(1) + item.slice(1).toLowerCase();
 // });
 
 // console.log(names);
-
-
 
 // const correctName = names.map(function(item){
 //   return item[0].toUpperCase() + item.slice(1).toLowerCase()
@@ -36,3 +31,36 @@
 // console.log(names)
 
 // console.log(correctName);
+
+/////////////select string ////////////////////
+
+// let forWords = [];
+
+// for (let i = 0; i < mix.length; i++) {
+//   if (typeof mix[i] === "string"&& isNaN(mix[i])) {
+//     forWords.push(mix[i]);
+//   }
+// }
+
+// console.log(forWords);
+
+// let filterWords = mix.filter((item) => {
+//   return typeof item === "string" && isNaN(item);
+// });
+
+// console.log(filterWords) ;
+
+
+///////////////////  filter positiveNumbers /////////////
+
+
+// let result = false;
+
+// for (let i = 0; i < mix.length; i++) {
+//   if (typeof mix[i] === "number") {
+//     result = true;
+//     break;
+//   }
+// }
+
+// console.log(result);
