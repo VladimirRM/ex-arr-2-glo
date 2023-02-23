1c27ea08087cf3aa54cc7fdf36a9ffd21a2f4abd
Vladimir RM <shvdrummer@gmail.com>
2023-02-23 14:06:48 +0200

    Correct Letter done

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const names = ["vladimir", "Mark", "kate", "Margo", "Peter"];
 const mix = [false, 0, NaN, 1, "Vladimir", "Khoma", 12];
 const badNum = [45, -7, "Mark", 2, 5, 7, 8];
 
-names.forEach((item, i, arr) => {
-  arr[i] = item[0].toUpperCase() + item.slice(1).toLowerCase();
+let correctName = names.map((item) => {
+  return item[0].toUpperCase() + item.slice(1).toLowerCase();
 });
 console.log(names);
+console.log(correctName);
