d6025e413b6829993d392d6f15f4033cce710ccd
Vladimir RM <shvdrummer@gmail.com>
2023-02-23 11:02:46 +0200

    for first Letter done

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const names = ["vladimir", "Mark", "kate", "Margo", "Peter"];
 const mix = [false, 0, NaN, 1, "Vladimir", "Khoma", 12];
 const badNum = [45, -7, "Mark", 2, 5, 7, 8];
 
-number.forEach((x, y, a) => {
-  console.log(number);
-});
+for (let i = 0; i < names.length; i++) {
+  names[i] = names[i][0].toUpperCase() + names[i].slice(1).toLowerCase();
+}
+
+console.log(names);
