a71a7f334f3773ff4ab6df3425d6aae56e540e4f
Vladimir RM <shvdrummer@gmail.com>
2023-02-23 08:43:33 +0200

    reduce done

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ const names = ["vladimir", "Mark", "kate", "Margo", "Peter"];
 const mix = [false, 0, NaN, 1, "Vladimir", "Khoma", 12];
 const badNum = [45, -7, "Mark", 2, 5, 7, 8];
 
-let sum = 0;
-
-for (let i = 0; i < number.length; i++) {
-  sum += number[i];
-}
+let sum = number.reduce((acc, next) => acc + next);
 
 console.log(sum);
