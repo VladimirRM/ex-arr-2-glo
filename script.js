7e296aa4fb390df73ecb76bbf798f0b6c4e46fe8
Vladimir RM <shvdrummer@gmail.com>
2023-02-23 08:37:27 +0200

    number with for done

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ const names = ["vladimir", "Mark", "kate", "Margo", "Peter"];
 const mix = [false, 0, NaN, 1, "Vladimir", "Khoma", 12];
 const badNum = [45, -7, "Mark", 2, 5, 7, 8];
 
+let sum = 0;
 
+for (let i = 0; i < number.length; i++) {
+  sum += number[i];
+}
+
+console.log(sum);
