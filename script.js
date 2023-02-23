8d9914174abcb51f3097498e03e29cc3ffbbc4b5
Vladimir RM <shvdrummer@gmail.com>
2023-02-23 13:53:54 +0200

    Names forEach done

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ const names = ["vladimir", "Mark", "kate", "Margo", "Peter"];
 const mix = [false, 0, NaN, 1, "Vladimir", "Khoma", 12];
 const badNum = [45, -7, "Mark", 2, 5, 7, 8];
 
-names.forEach((item, index, arr) => {
-  arr[index] = item[0].toUpperCase(1) + item.slice(1).toLowerCase();
+names.forEach((item, i, arr) => {
+  arr[i] = item[0].toUpperCase() + item.slice(1).toLowerCase();
 });
-
 console.log(names);
