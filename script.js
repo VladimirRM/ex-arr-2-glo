d8fe92d78bdc00c9b213800e2ea79218e53ff7a5
Vladimir RM <shvdrummer@gmail.com>
2023-02-24 14:27:25 +0200

    map done

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const names = ["vladimir", "Mark", "kate", "Margo", "Peter"];
 const mix = [false, 0, NaN, 1, "Vladimir", "Khoma", 12];
 const badNum = [45, -7, "Mark", 2, 5, 7, 8];
 
-names.forEach((item, i, arr) => {
-  arr[i] = item[0].toUpperCase() + item.slice(1).toLowerCase();
-});
 
-console.log(names);
+
+let correctName = names.map((item)=>{
+   return item[0].toUpperCase() + item.slice(1).toLowerCase()
+})
+
+console.log(correctName);
