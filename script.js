97c172be08f93bbffa297457656ca4072b7450b3
Vladimir RM <shvdrummer@gmail.com>
2023-02-26 08:44:29 +0200

    number in array true done

--- script.js
+++ script.js
@@ -3,12 +3,7 @@ const names = ["vladimir", "Mark", "kate", "Margo", "Peter"];
 const mix = [false, 0, NaN, 1, "Vladimir", "Khoma", 12];
 const badNum = [45, -7, "Mark", 2, 5, 7, 8];
 
-let forWords = [];
-
-for (let i = 0; i < mix.length; i++) {
-  if (typeof mix[i] === "string") {
-    forWords.push(mix[i]);
-  }
-}
-
-console.log(forWords);
+let badResult = badNum.every((item) => {
+  return typeof item === "number";
+});
+console.log(badResult);
