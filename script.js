18a3a161ef2f5d552d8b842ae38b3b7b9b69a24c
Vladimir RM <shvdrummer@gmail.com>
2023-02-26 11:34:35 +0200

    Letter main  with for done

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const names = ["vladimir", "Mark", "kate", "Margo", "Peter"];
 const mix = [false, 0, NaN, 1, "Vladimir", "Khoma", 12];
 const badNum = [45, -7, "Mark", 2, 5, 7, 8];
 
-let sumArr = [];
+// lowerCase for foreach map mix /////////
+/////////////select string  or type//
 
-sumArr = number.concat(names).slice(1);
+for (let i = 0; i < names.length; i++) {
+  names[i] = names[i][0].toUpperCase() + names[i].slice(1).toLowerCase();
+}
 
-console.log(sumArr);
+console.log(names);
