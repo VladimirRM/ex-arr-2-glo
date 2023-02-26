0fbd0308158a56926f7d0e700a66512d737f161a
Vladimir RM <shvdrummer@gmail.com>
2023-02-26 08:47:24 +0200

    every number done

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const names = ["vladimir", "Mark", "kate", "Margo", "Peter"];
 const mix = [false, 0, NaN, 1, "Vladimir", "Khoma", 12];
 const badNum = [45, -7, "Mark", 2, 5, 7, 8];
 
-let badResult = badNum.every((item) => {
+let badResult = number.every((item) => {
   return typeof item === "number";
 });
+
 console.log(badResult);
