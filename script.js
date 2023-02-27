c180603390db4fe0c1daa8387501cf1faa160640
Vladimir RM <shvdrummer@gmail.com>
2023-02-27 09:59:03 +0200

    select string from arr done

--- script.js
+++ script.js
@@ -3,15 +3,11 @@ const names = ["vladimir", "Mark", "kate", "Margo", "Peter"];
 const mix = [false, 0, NaN, 1, "Vladimir", "Khoma", 12];
 const badNum = [45, -7, "Mark", 2, 5, 7, 8];
 
-// lowerCase for foreach map mix /////////
-/////////////select string  or type//
+// lowerCase for foreach map names /////////
+/////////////select type "string" from mix, for,filter //
 
-let forWords = [];
+let result = mix.filter((item) => {
+  return typeof item === "string";
+});
 
-for (let i = 0; i < mix.length; i++) {
-  if (typeof mix[i] === "string") {
-    forWords.push(mix[i]);
-  }
-}
-
-console.log(forWords);
+console.log(result);
