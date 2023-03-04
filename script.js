fb8f3c5de4182186ff5246dd1d9b441db8330cd4
Vladimir RM <shvdrummer@gmail.com>
2023-03-04 10:47:55 +0200

    Main letter forEach done

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ const badNum = [45, -7, "Mark", 2, 5, 7, 8];
 // lowerCase for foreach map names /////////
 /////////////select type "string" from mix, for,filter //
 
-for (let i = 0; i < names.length; i++) {
-  names[i] = names[i][0].toUpperCase() + names[0].slice(1).toLowerCase();
-}
-
+names.forEach((item,i,arr)=>{
+  return arr[i] = item[0].toUpperCase() + item.slice(1).toLowerCase()
+})
 console.log(names);
