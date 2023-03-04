f1868a5dbce42034911136163f5156551295d4b3
Vladimir RM <shvdrummer@gmail.com>
2023-03-04 10:41:55 +0200

    start app

--- script.js
+++ script.js
@@ -6,8 +6,4 @@ const badNum = [45, -7, "Mark", 2, 5, 7, 8];
 // lowerCase for foreach map names /////////
 /////////////select type "string" from mix, for,filter //
 
-let correctNames = names.map((item, i, arr) => {
-  return (arr[i] = item[0].toUpperCase() + item.slice(1).toLowerCase());
-});
-
-console.log(correctNames);
+let result = false;
