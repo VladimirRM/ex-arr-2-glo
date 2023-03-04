4a7022052819fe7a9bba5f3b3bfa36acba1628df
Vladimir RM <shvdrummer@gmail.com>
2023-03-04 11:07:33 +0200

    Type string in array with  for done

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ const badNum = [45, -7, "Mark", 2, 5, 7, 8];
 // lowerCase for foreach map names /////////
 /////////////select type "string" from mix, for,filter //
 
-let correctName = names.map((item, i, arr) => {
-  return (arr[i] = item[0].toUpperCase() + item.slice(1).toLocaleLowerCase());
-});
+let result = [];
 
-console.log(correctName);
+for (let i = 0; i < mix.length; i++) {
+  if (typeof mix[i] === "string") {
+    result.push(mix[i]);
+  }
+}
+
+console.log(result);
