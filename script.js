36956e78cb6a03fc0403ff90818998443f8309cf
Vladimir RM <shvdrummer@gmail.com>
2023-03-11 09:51:32 +0200

    find type string in arr with for done

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ const mix = [false, 0, NaN, 1, "Vladimir", "Khoma", 12];
 const badNum = [45, -7, "Mark", 2, 5, 7, 8];
 
 // lowerCase for foreach map names /////////
+//////////find type string into mix with for //////////
 /////////////select type "string" from mix, for,filter////////
 
-let cor = names.map((item, i, arr) => {
-  return (arr[i] = item[0].toUpperCase() + item.slice(1).toLowerCase());
-});
+let result = [];
 
-console.log(cor);
+for (let i = 0; i < mix.length; i++) {
+  if (typeof mix[i] === "string") {
+    result.push(mix[i]);
+  }
+}
+console.log(result);
