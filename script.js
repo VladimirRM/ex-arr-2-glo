0ac0426e81f3861cf521530fa6c2dbc9d959862e
Vladimir RM <shvdrummer@gmail.com>
2023-03-13 10:31:19 +0200

    correct first letter with forEach done

--- script.js
+++ script.js
@@ -7,8 +7,12 @@ const badNum = [45, -7, "Mark", 2, 5, 7, 8];
 //////////find type string into mix with for //////////
 /////////////select type "string" from mix, for,filter////////
 
-for (let i = 0; i < names.length; i++) {
-  names[i] = names[i][0].toUpperCase() + names[i].slice(1).toLowerCase();
-}
+names.forEach((item, i, arr) => {
+  return (arr[i] = item[0].toUpperCase() + item.slice(1).toLowerCase());
+});
 
 console.log(names);
+
+// for (let i = 0; i < names.length; i++) {
+
+// }
