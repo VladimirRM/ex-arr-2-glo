92b00bf56a5b92d3a63ba5562c8c752898ac3cd5
Vladimir RM <shvdrummer@gmail.com>
2023-03-17 08:49:51 +0200

    Correct names ising for done

--- script.js
+++ script.js
@@ -7,15 +7,12 @@ const badNum = [45, -7, "Mark", 2, 5, 7, 8];
 //////////find type string in mix with for //////////
 /////////////select type "string" from mix, for,filter////////
 
-let res = [];
-
-for (let i = 0; i < mix.length; i++) {
-  if (typeof mix[i] === "string") {
-    res.push(mix[i]);
-  }
+// let res = [];
+for (let i = 0; i < names.length; i++) {
+  names[i] = names[i][0].toUpperCase() + names[i].slice(1).toLowerCase();
 }
 
-console.log(res);
+console.log(names);
 
 // for (let i = 0; i < names.length; i++) {
 
