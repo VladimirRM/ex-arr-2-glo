f18747fc9f6bac922ad7508139ec956dbda805ef
Vladimir RM <shvdrummer@gmail.com>
2023-03-19 09:16:01 +0200

    Find string in arr done

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ const badNum = [45, -7, "Mark", 2, 5, 7, 8];
 // lowerCase for foreach map names /////////
 //////////find type string in mix with for //////////
 /////////////select type "string" from mix, for,filter////////
+let res = [];
 
-names.forEach((x, y, z) => {
-  return (z[y] = x[0].toUpperCase() + x.slice(1).toLowerCase());
-});
-console.log(names);
+for (let i = 0; i < mix.length; i++) {
+  if (typeof mix[i] === "string") res.push(mix[i]);
+}
+console.log(res);
 
 // for (let i = 0; i < names.length; i++) {
 
