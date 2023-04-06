b6e7ce0614d5dda8a1b8d51552995acaea8a86bf
Vladimir RM <shvdrummer@gmail.com>
2023-04-06 11:56:50 +0300

    Find type in arr u some.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const number = [12, -3, 56, 76, 45, -1];
-const names = ["vladimir", "Mark", "kate", "Margo", "Peter"];
+const names = ["vladimir", "mark", "kate", "Margo", "Peter"];
 const mix = [false, 0, NaN, 1, "Vladimir", "Khoma", 12];
 const badNum = [45, -7, "Mark", 2, 5, 7, 8];
 const arr = ["vladimir", "Mark", "kate", "Margo", "Peter", "Nikolas"];
@@ -10,14 +10,14 @@ const arr = ["vladimir", "Mark", "kate", "Margo", "Peter", "Nikolas"];
 /////////////recursion///////////////
 /////////////check type use every & some /////////
 
-function printArr(arr, index = 0) {
-  console.log(arr[index]);
-  index = index + 1;
-  if (index < arr.length) {
-    printArr(arr, index);
-  }
-}
+// names.forEach((x,y,z)=>{
+//   return
+// })
 
-printArr(arr);
+let res = mix.some((item) => {
+  return typeof item === "string";
+});
+
+console.log(res);
 
 // for (let i = 0; i < names.length; i++) {}
